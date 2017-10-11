7ea30e7af1fffb3bfa55facfd31eb035b67d4a91
cosmicray01 <josemijangos831@gmail.com>
2017-10-11 21:16:37 +0000

    adding

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+
+        <h1> Welcome to Jose Mijangos' CST336 website</h1>
+        <nav>
+            <ul>
+                <li><a id="current" href="index.php">Home </a></li>
+                <li><a href="/cst336workspace/Homework/homework3/dice-game/diceGame.php">Dice Game</a></li>
+            </ul>
+            <figure id="cpp">
+                    <img src="cpp.png" width=200>
+            </figure>
+        </nav>
+
+
+    </body>
+</html>
