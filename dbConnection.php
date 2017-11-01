6785e8f032ed0d988d212c791eac28020758c983
cosmicray01 <josemijangos831@gmail.com>
2017-11-01 08:37:54 +0000

    adding

--- dbConnection.php
+++ dbConnection.php
@@ -11,10 +11,10 @@ function getDatabaseConnection($dbname = 'tcp'){
     //using different database variables in Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
+        $host = $url["us-cdbr-iron-east-03.cleardb.net"];
+        $dbname = substr($url["heroku_e025c2bcd9ffba75c"], 1);
+        $username = $url["b4ec28ac117d04"];
+        $password = $url["e77c8602"];
     } 
     
     //creates db connection
